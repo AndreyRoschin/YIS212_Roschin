eaafecdbc8053b30a8bc3cac135201eeb8b69870
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:04:14 +0300

    Update Cursor.cpp

--- Task1-2Var20/Cursor.cpp
+++ Task1-2Var20/Cursor.cpp
@@ -1,19 +1,18 @@
 #include"Cursor.h"
 
-void Cursor::set_correct_coordinates(const unsigned int& coordinates_x, const unsigned int& coordinates_y)
+void Cursor::set_correct_coordinates(const int coordinates_x, const int coordinates_y)
 {
 	if (coordinates_x >= START_SCREEN_X || coordinates_x <= END_SCREEN_X || coordinates_y >= START_SCREEN_Y || coordinates_y <= END_SCREEN_Y)
 	{
+		throw invalid_argument("Incorrect coordinates");
 		this->coordinates_x = coordinates_x;
 		this->coordinates_y = coordinates_y;
 	}
-	else
-	{
-		throw invalid_argument("Incorrect coordinates");
-	}
+	this->coordinates_x = coordinates_x;
+	this->coordinates_y = coordinates_y;
 }
 
-void Cursor::set_correct_cursor_size(const unsigned int& cursor_size)
+void Cursor::set_correct_cursor_size(const int cursor_size)
 {
 	if (cursor_size < MIN_CURSOR_SIZE || cursor_size > MAX_CURSOR_SIZE )
 	{
@@ -26,15 +25,13 @@ void Cursor::set_correct_cursor_type(const string& cursor_type)
 {
 	if (cursor_type == "horizontal" || cursor_type == "vertical")
 	{
+		throw invalid_argument("Cursor must be horizontal or vertical");
 		this->cursor_type = cursor_type;
 	}
-	else
-	{
-		throw invalid_argument("Cursor must be horizontal or vertical");
-	}
+	this->cursor_type = cursor_type;
 }
 
-Cursor::Cursor(const unsigned int& coordinates_x, const unsigned int& coordinates_y, const unsigned int& cursor_size, const string& cursor_type, const bool& cursor_visibility)
+Cursor::Cursor(const unsigned int coordinates_x, const unsigned int coordinates_y, const unsigned int cursor_size, const string& cursor_type, const bool cursor_visibility)
 {
 	set_correct_coordinates(coordinates_x, coordinates_y);
 	set_correct_cursor_size(cursor_size);
