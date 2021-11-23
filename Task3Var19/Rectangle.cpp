690351135c50f780ed3e948c956176cd824bb779
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:12:51 +0300

    Update Rectangle.cpp

--- Task3Var19/Rectangle.cpp
+++ Task3Var19/Rectangle.cpp
@@ -1,22 +1,22 @@
 #include "Rectangle.h"
 
-Rectangle::Rectangle(const double& length, const double& width) : Pair(length, width)
+Rectangle::Rectangle(const double& length, const double& width)
 {
 	set_correct_length(length);
 	set_correct_width(width);
 }
 
-double Rectangle::get_area()
+double Rectangle::get_area()const
 {
 	return this->width * this->length;
 }
 
-double Rectangle::get_perimeter()
+double Rectangle::get_perimeter()const
 {
 	return 2 * this->length + 2 * this->length;
 }
 
-void Rectangle::set_correct_length(const double& length)
+void Rectangle::set_correct_length(const double length)
 {
 	if (length < 0)
 	{
@@ -25,7 +25,7 @@ void Rectangle::set_correct_length(const double& length)
 	this->length = length;
 }
 
-void Rectangle::set_correct_width(const double& width)
+void Rectangle::set_correct_width(const double width)
 {
 	if (width < 0)
 	{
