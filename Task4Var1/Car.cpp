ca297e35b80f554e96336b0b8e9851e5554fd14c
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:22:45 +0300

    Update Car.cpp

--- Task4Var1/Car.cpp
+++ Task4Var1/Car.cpp
@@ -1,26 +1,20 @@
 #include "Car.h"
 using namespace std;
-void Car::set_number_of_passengers(const int& number_of_passengers)
+void Car::set_number_of_passengers(const int number_of_passengers)
 {
-	if (number_of_passengers > 0 && number_of_passengers <= 5)
-	{
-		this->number_of_passengers = number_of_passengers;
-	}
-	else
+	if (number_of_passengers > MIN_PASSENGERS && number_of_passengers <= MAX_PASSENGERS)
 	{
 		throw invalid_argument("Passengers can be from 1 to 5");
 	}
+	this->number_of_passengers = number_of_passengers;
 }
-void Car::set_fuel(const double& fuel)
+void Car::set_fuel(const double fuel)
 {
 	if (fuel > 0)
 	{
-		this->fuel = fuel;
-	}
-	else
-	{
 		throw invalid_argument("Fuel must be positive");
 	}
+	this->fuel = fuel;
 }
 Car::Car()
 {
@@ -29,7 +23,7 @@ Car::Car()
 	number_of_passengers = 1;
 	fuel = 1;
 }
-Car::Car(const double& average_speed, const string& model, const int& number_of_passengers, const double& distance, const double& fuel)
+Car::Car(const double average_speed, const string& model, const int number_of_passengers, const double distance, const double fuel)
 {
 	set_average_speed(average_speed);
 	this->model = model;
@@ -37,19 +31,19 @@ Car::Car(const double& average_speed, const string& model, const int& number_of_
 	set_distance(distance);
 	set_fuel(fuel);
 }
-double Car::get_fuel()
+double Car::get_fuel()const
 {
 	return fuel;
 }
-double Car::get_fuel_consumption(const double& q)
+double Car::get_fuel_consumption(const double q)
 {
 	return q * distance / 100;
 }
-void Car::set_new_fuel(const double& fuel)
+void Car::set_new_fuel(const double fuel)
 {
 	set_fuel(fuel);
 }
-void Car::set_new_distance(const double& distance)
+void Car::set_new_distance(const double distance)
 {
 	set_distance(distance);
 }
