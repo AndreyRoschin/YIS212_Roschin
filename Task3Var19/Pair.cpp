165ae1b5123de062f91a8051363824111137b40f
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:10:09 +0300

    Update Pair.cpp

--- Task3Var19/Pair.cpp
+++ Task3Var19/Pair.cpp
@@ -1,6 +1,6 @@
 #include "Pair.h"
 
-Pair::Pair(const double& first_number, const double& second_number)
+Pair::Pair(const double first_number, const double second_number)
 {
 	this->first_number = first_number;
 	this->second_number = second_number;
@@ -8,27 +8,27 @@ Pair::Pair(const double& first_number, const double& second_number)
 
 Pair::~Pair() = default;
 
-double Pair::get_first_number()
+double Pair::get_first_number()const
 {
 	return this->first_number;
 }
 
-double Pair::get_second_number()
+double Pair::get_second_number()const
 {
 	return this->second_number;
 }
 
-double Pair::get_multiply()
+double Pair::get_multiply()const
 {
 	return this->first_number * this->second_number;
 }
 
-void Pair::set_first_number(const double& first_number)
+void Pair::set_first_number(const double first_number)
 {
 	this->first_number = first_number;
 }
 
-void Pair::set_second_number(const double& second_number)
+void Pair::set_second_number(const double second_number)
 {
 	this->second_number = second_number;
 }
