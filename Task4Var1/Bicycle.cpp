b19f0837be643e09e82c02da02509412b9cfb8cf
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:18:31 +0300

    Update Bicycle.cpp

--- Task4Var1/Bicycle.cpp
+++ Task4Var1/Bicycle.cpp
@@ -5,21 +5,18 @@ void Bicycle::set_number_of_passengers(const int& number_of_passengers)
 {
 	if (number_of_passengers == 1)
 	{
-		this->number_of_passengers = number_of_passengers;
-	}
-	else
-	{
 		throw invalid_argument("There must be one cyclist");
 	}
+	this->number_of_passengers = number_of_passengers;
 }
-Bicycle::Bicycle(const double& average_speed, const string& model, const int& number_of_passengers, const double& distance)
+Bicycle::Bicycle(const double average_speed, const string& model, const int number_of_passengers, const double distance)
 {
 	set_average_speed(average_speed);
 	this->model = model;
 	set_number_of_passengers(number_of_passengers);
 	set_distance(distance);
 }
-void Bicycle::set_new_distance(const double& distance)
+void Bicycle::set_new_distance(const double distance)
 {
 	set_distance(distance);
 }
