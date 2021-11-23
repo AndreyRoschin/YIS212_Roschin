872e2cab1642ceb52f77d0b836c1b512c62ddadb
AndreyRoschin <92104137+AndreyRoschin@users.noreply.github.com>
2021-11-23 18:11:47 +0300

    Update Pair.h

--- Task3Var19/Pair.h
+++ Task3Var19/Pair.h
@@ -7,32 +7,34 @@ class Pair
 public:
 	/**
 	 \brief	Конструктор с параметрами
+	 \params first_number
+	 \params second_number
 	 */
-	Pair(const double& first_number, const double& second_number);
+	Pair(const double first_number, const double second_number);
 	/**
 	 \brief Деструктор
 	 */
-	~Pair();
+	virtual ~Pair();
 	/**
 	 \brief Получение первого числа
 	 */
-	double get_first_number();
+	double get_first_number()const;
 	/**
 	 \brief Получение второго числа
 	 */
-	double get_second_number();
+	double get_second_number()const;
 	/**
 	 \brief Получение произведения чисел
 	 */
-	double get_multiply();
+	double get_multiply()const;
 	/**
 	 \brief Изменение первого числа
 	 */
-	void set_first_number(const double& first_number);
+	void set_first_number(const double first_number);
 	/**
 	 \brief Изменение второго числа
 	 */
-	void set_second_number(const double& second_number);
+	void set_second_number(const double second_number);
 private:
 	double first_number;
 	double second_number;
